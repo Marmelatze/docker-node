3b083096834ff4ec258b7f7e9dab03d23fabfc4d
Florian Pfitzer <pfitzer@wurzel3.de>
2015-05-26 16:25:34 +0200

    docker cleanup

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-var _ = require("lodash");
-var consul = require('consul')({
-    host: "10.244.86.1"
-});
+'use strict';
 
 var Promise = require("bluebird");
+
+var _ = require("lodash");
+
+Promise.promisifyAll(require("consul"));
+Promise.promisifyAll(require("consul/lib/agent"));
+Promise.promisifyAll(require("consul/lib/agent/service"));
+
+var consul = require("consul")({
+    host: "consul"
+});
+
+
 Promise.promisifyAll(require("dockerode").prototype);
 Promise.promisifyAll(require("dockerode/lib/container").prototype);
 
-
 var Docker = require("dockerode");
-var docker = new Docker({socketPath: '/var/run/docker.sock'});
+var docker = new Docker({socketPath: "/var/run/docker.sock"});
 
 var oldInfluxdbIp = undefined;
 function startCadvisor(influxdb) {
@@ -22,17 +30,18 @@ function startCadvisor(influxdb) {
     var container = docker.getContainer("cadvisor");
     container
         .stopAsync()
-        .catch(function(e) {})
-        .then(function(result) {
+        .catch(function (e) {
+        })
+        .then(function (result) {
             return container.removeAsync();
         })
-        .catch(function(e) {
-            console.log(e);
+        .catch(function (e) {
+            console.error(e);
         })
-        .then(function() {
+        .then(function () {
             return docker.createContainerAsync({
-                Image: "google/cadvisor", 
-                Cmd: ['-storage_driver=influxdb', '-storage_driver_host='+influxdb.Address+":"+influxdb.Port],
+                Image: "google/cadvisor",
+                Cmd: ["-storage_driver=influxdb", "-storage_driver_host=" + influxdb.Address + ":" + influxdb.Port],
                 name: "cadvisor",
                 Tty: false,
                 HostConfig: {
@@ -43,9 +52,9 @@ function startCadvisor(influxdb) {
                         "/var/lib/docker/:/var/lib/docker:ro"
                     ]
                 }
-            }); 
+            });
         })
-        .then(function(container) {
+        .then(function (container) {
             console.log("container id", container.id);
             return container.startAsync();
         });
@@ -63,13 +72,14 @@ function startCadvisor(influxdb) {
 //    }
 //});
 
-var watch = consul.watch({method: consul.catalog.service.nodes, options: { service: "influxdb" }}); 
-watch.on("change", _.debounce(function(data) {
+/*
+var watch = consul.watch({method: consul.catalog.service.nodes, options: {service: "influxdb"}});
+watch.on("change", _.debounce(function (data) {
     console.log("change");
     //console.log(data);
 
     // get healthy influxdb
-    consul.health.service({service: "influxdb", passing: 1}, function(err, result) {
+    consul.health.service({service: "influxdb", passing: 1}, function (err, result) {
         for (var i = 0; i < result.length; i++) {
             var check = result[i];
             var service = check.Service;
@@ -81,4 +91,46 @@ watch.on("change", _.debounce(function(data) {
     });
 
 }, 1000));
+*/
+function cleanup() {
+    console.log("running cleanup on local node");
+
+    Promise.join(
+        docker.listContainersAsync(),
+        consul.agent.servicesAsync(),
+        function(containers, services) {
+            services = _.filter(services[0], function(service) {
+                var containerName = service.ID.split(":")[1];
+                service.containerName = containerName;
+                return undefined !== containerName;
+            });
+
+            var containerIndex = _.map(containers, function(container) {
+                return container.Names[0].substr(1);
+            });
+
+            var promises = [];
+            _.each(services, function(service) {
+                if (containerIndex.indexOf(service.containerName) !== -1) {
+                    return;
+                }
+
+                console.log("Delete service " + service.ID);
+                promises.push(consul.agent.service.deregisterAsync(service.ID));
+            });
+
+            return promises;
+        }
+    ).done(function() {
+        console.log("done");
+    });
+};
+
+cleanup();
+
+
+// cleanup
+setInterval(function () {
+    cleanup();
+}, 1000 * 60 * 5);
 
